482733b2a4b7faa738b779966929dd167414edd4
Pranav Hundekari <@PranavHun>
2023-09-26 18:06:11 +0530

    added config file

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Python Environment Activation
+# Python Virtual Environment Activation
 
-A CLI tool to list python environments and select and activate one.
+A CLI tool to list python virtual environments and select and activate one.
 The path to the environments can be provided through a config file.
 The tool tries to identify the shell you are currently in and selects 
 the appropriate activation file.
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,88 @@
 use std::process::Command;
+
+fn text_bold<T: std::fmt::Display>(text: T) -> String {
+    format!("\x1b[37;1m{text}\x1b[0m")
+}
+
+fn get_environments(search_dir: &str) -> Vec<String> {
+    let py_env_directories = String::from_utf8(
+        Command::new("find")
+            .args([search_dir, "-iname", "pyvenv.cfg"])
+            .output()
+            .expect("fd command didn't run")
+            .stdout,
+    )
+    .expect("fd command didn't produce and output.");
+    let mut environment_directories = Vec::<String>::new();
+    for path in py_env_directories.split("\n") {
+        if !path.is_empty() {
+            let mut path_arr: Vec<&str> = path.split("/").collect();
+            path_arr.pop();
+            path_arr.push("bin");
+            path_arr.push("activate");
+            let environment_directory = path_arr.join("/");
+            environment_directories.push(environment_directory);
+        }
+    }
+    environment_directories
+}
+
+fn get_shell() -> String {
+    let current_exe_path = std::env::current_exe().expect("Cannot read current executable name.");
+    let current_exe = current_exe_path
+        .components()
+        .last()
+        .expect("Cannot find executable name.")
+        .as_os_str()
+        .to_str()
+        .expect("Cannot convert executable name to str.");
+
+    let currently_running_processes = String::from_utf8(
+        Command::new("ps")
+            .args(["-o", "cmd"])
+            .output()
+            .expect("ps command didn't run")
+            .stdout,
+    )
+    .expect("ps command didn't produce and output.");
+    let mut shell = String::new();
+    for line in currently_running_processes.split("\n") {
+        if line.contains("ps -o cmd") {
+            break;
+        } else {
+            if line.contains(current_exe) {
+                continue;
+            }
+            shell = String::from(line);
+        }
+    }
+    shell
+}
+
 fn main() {
     let _ = match std::env::consts::FAMILY {
         "unix" => {
-            let currently_running_processes = String::from_utf8(
-                Command::new("ps")
-                    .args(["-o", "cmd"])
-                    .output()
-                    .expect("ps command didn't run")
-                    .stdout,
-            )
-            .expect("ps command didn't produce and output.");
-            let mut shell = "";
-            for line in currently_running_processes.split("\n") {
-                if line.contains("ps -o cmd") {
-                    break;
-                } else {
-                    if line.contains("pyenvselect") {
-                        continue;
+            let shell = get_shell();
+            println!("Current Shell : {}", text_bold(&shell));
+            println!("");
+
+            let suffix = if shell.contains("fish") { ".fish" } else { "" };
+
+            let search_dirs = std::fs::read_to_string(".config/pyvenvselect/searchdirs")
+                .unwrap_or(String::from("not found"));
+            let mut env_number = 1;
+
+            for search_dir in search_dirs.split("\n") {
+                if !search_dir.is_empty() {
+                    let environments = get_environments(search_dir);
+                    println!("Environments at : {}", text_bold(search_dir));
+                    for env in environments {
+                        println!("[{env_number}] - {env}{suffix}");
+                        env_number += 1;
                     }
-                    shell = line;
+                    println!();
                 }
             }
-
-            println!("Shell is : {shell}");
             // TODO: Using the shell, activate
             // the env file
         }
